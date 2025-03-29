c06c2ef99f56565507ac6b37659bd797242f7318
Jadonamite <jadonamite@gmail.com>
2025-03-29 06:52:02 +0100

    styling the game

--- Memory-Game/src/components/LoadingPage.jsx
+++ Memory-Game/src/components/LoadingPage.jsx
@@ -6,7 +6,7 @@ const LoadingPage = () => {
       <div className="loading-container">
          <div className="loading-content">
             <h2 className="loading-title">WELCOME</h2>
-            <div className="pulsing-box"></div>
+            <div className="pulsing-box text-white">Web3Nova</div>
             <div className="loading-footer">Built by Web3Nova</div>
          </div>
       </div>
